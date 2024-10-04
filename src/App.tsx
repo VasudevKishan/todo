61a3976739168a2a6db99f42cb2760f2331716cb
Vasudev Kishan <vasudevkishan@gmail.com>
2024-10-04 12:36:52 +0530

    add background color variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <h1>
-      todo.
-      <p>{import.meta.env.VITE_EXAMPLE_ENV_VAR}</p>
-      Coming Soon.....!
-    </h1>
+    <>
+      <h1>
+        todo.
+        <p>{import.meta.env.VITE_EXAMPLE_ENV_VAR}</p>
+        Coming Soon.....!
+      </h1>
+    </>
   );
 }
 
