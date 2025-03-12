80f2c46f82d5ed773819f7bf5207680e7cd44a5d
Vasudev Kishan <vasudevkishan@gmail.com>
2025-03-12 22:41:34 +0530

    Initial setup

---
+++ src/header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header: React.FC = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+            {/* Add your components and logic here */}
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/todo-container/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoContainer: React.FC = () => {
+    return (
+        <div>
+            <h1>Todo List</h1>
+            {/* Add your components and logic here */}
+        </div>
+    );
+};
+
+export default TodoContainer;
