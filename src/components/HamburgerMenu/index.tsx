aa9d40d9386e4501f425f18e2f3b057ad25fe097
Vasudev Kishan <vasudevkishan@gmail.com>
2025-03-16 10:31:22 +0530

    module specific styles for hamburger menu

--- src/components/HamburgerMenu/index.tsx
+++ src/components/HamburgerMenu/index.tsx
@@ -1,16 +1,16 @@
 import React, {useRef} from "react";
-import "./styles.css";
+import styles from "./styles.module.css";
 
 const HamburgerMenu: React.FC = () => {
     const menu = useRef<HTMLButtonElement>(null);
     //Todo: remove this function below
     const toggleSidebar = () => {
-        menu.current?.classList.toggle("active");
+        menu.current?.classList.toggle(styles.active);
     };
     return (
-        <button className="icon" ref={menu} onClick={toggleSidebar}>
-            <div className="line line1"></div>
-            <div className="line line2"></div>
+        <button className={styles.icon} ref={menu} onClick={toggleSidebar}>
+            <div className={`${styles.line} ${styles.line1}`}></div>
+            <div className={`${styles.line} ${styles.line2}`}></div>
         </button>
     );
 };
