c8381cfebf781645f78914a3fbbc871853733d23
Vasudev Kishan <vasudevkishan@gmail.com>
2025-03-16 10:41:28 +0530

    create new todo container and add module specific styles

---
+++ src/todo-contiainer/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const TodoContainer: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Todos</h1>
+        </div>
+    );
+};
+
+export default TodoContainer;
