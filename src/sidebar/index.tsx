351481f918e73d65f38ee6ecaf3bcb0fb2b24659
Vasudev Kishan <vasudevkishan@gmail.com>
2025-03-18 00:13:43 +0530

    create new filter title component (Maybe rename it later) used to add filters buttons in filter list

--- src/sidebar/index.tsx
+++ src/sidebar/index.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./styles.module.css";
+import FilterTitle from "../components/FilterTitle";
+
+type filterTypes = "All" | "Starred";
 
 const SideBar: React.FC = () => {
+    const [activeFilter, setActiveFilter] = useState<filterTypes>("All");
+
+    const handleFilterClick = (filterName: filterTypes) => {
+        setActiveFilter(filterName);
+        console.log("All todos");
+    };
     return (
         <aside className={styles.container}>
             <div className={styles.filters}>
-                <h2>Filters</h2>
+                <div className={styles.filterTitleGroup}>
+                    <h2>Filters</h2>
+                    <input type="checkbox" />
+                </div>
+                <div className={styles.filterList}>
+                    <FilterTitle
+                        filterName="All"
+                        onClickHandler={() => handleFilterClick("All")}
+                        icon="all_inbox"
+                        isActive={activeFilter === "All"}
+                    />
+                    <FilterTitle
+                        filterName="Starred"
+                        onClickHandler={() => handleFilterClick("Starred")}
+                        icon="star"
+                        isActive={activeFilter === "Starred"}
+                    />
+                </div>
             </div>
         </aside>
     );
