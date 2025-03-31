75b9ecd23913438adc6957f536b9d79b8ae11bae
Vasudev Kishan <vasudevkishan@gmail.com>
2025-03-31 18:16:46 +0530

    Add slides for view detail and add task

--- src/todo-contiainer/index.tsx
+++ src/todo-contiainer/index.tsx
@@ -5,19 +5,45 @@ import {ActionButton} from "../components/ActionButton/ActionButton";
 const TodoContainer: React.FC = () => {
     return (
         <main className={styles.container}>
-            <ActionButton
-                varient="primary"
-                onClick={() => console.log("Clicked on primary btn")}
-            >
-                Primary Btn
-            </ActionButton>
-            <br />
-            <ActionButton
-                varient="secondary"
-                onClick={() => console.log("Clicked on Secondary btn")}
-            >
-                <span className="material-icons">arrow_back_ios</span>
-            </ActionButton>
+            <div className={`${styles.slide} ${styles["add-task"]}`}>
+                Add Task
+                <ActionButton
+                    className={styles.backBtn}
+                    varient="secondary"
+                    onClick={() => console.log("Clicked on Secondary btn")}
+                >
+                    <span className="material-icons">arrow_back_ios</span>
+                </ActionButton>
+                <ActionButton
+                    varient="primary"
+                    onClick={() => console.log("Clicked on primary btn")}
+                    className={styles.addBtn}
+                >
+                    Add
+                </ActionButton>
+            </div>
+
+            <div className={`${styles.slide} ${styles["view-task"]}`}>
+                View Tasks
+                <ActionButton
+                    varient="primary"
+                    onClick={() => console.log("Clicked on primary btn")}
+                    className={styles.addBtn}
+                >
+                    Add Task
+                </ActionButton>
+            </div>
+
+            <div className={`${styles.slide} ${styles["task-detail"]}`}>
+                Task Detail
+                <ActionButton
+                    className={styles.backBtn}
+                    varient="secondary"
+                    onClick={() => console.log("Clicked on Secondary btn")}
+                >
+                    <span className="material-icons">arrow_back_ios</span>
+                </ActionButton>
+            </div>
         </main>
     );
 };
