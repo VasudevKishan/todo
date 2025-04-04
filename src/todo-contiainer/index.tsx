fc0900d9a9daa50e03c40b580e7ad7123b3e0863
Vasudev Kishan <vasudevkishan@gmail.com>
2025-04-04 23:18:21 +0530

    Add taskDetail view with styles

--- src/todo-contiainer/index.tsx
+++ src/todo-contiainer/index.tsx
@@ -77,19 +77,61 @@ const TodoContainer: React.FC = forwardRef<HTMLDivElement>((_, ref) => {
                 </ActionButton>
             </div>
 
-            <div className={`${styles.slide} ${styles["task-detail"]}`}>
-                <h2 className={styles.title}>Task Details</h2>
-                <h2 className={styles.taskTitle}>{selectedTask?.title}</h2>
-                <h2 className={styles.taskDescription}>
-                    {selectedTask?.description}
-                </h2>
-                <ActionButton
-                    className={styles.backBtn}
-                    varient="secondary"
-                    onClick={() => changeAction("view")}
-                >
-                    <span className="material-icons">arrow_back_ios</span>
-                </ActionButton>
+            <div
+                className={`${styles.slide} ${styles.taskDetail} ${styles["task-detail"]}`}
+            >
+                <div className={styles.expandTitle}>
+                    <h2 className={styles.title}>Task Details</h2>
+                </div>
+
+                <div className={styles.detailHeader}>
+                    <h2 className={styles.taskTitle}>{selectedTask?.title}</h2>
+                    <div className={styles.icons}>
+                        {selectedTask?.starred ? (
+                            <span className={`material-icons ${styles.icon}`}>
+                                star
+                            </span>
+                        ) : (
+                            ""
+                        )}
+
+                        {selectedTask?.completed ? (
+                            <span className={`material-icons ${styles.icon}`}>
+                                check_circle
+                            </span>
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                </div>
+
+                <div className={styles.detailContent}>
+                    <hr className={styles.divider}></hr>
+                    <p className={styles.taskDescription}>
+                        {selectedTask?.description ? (
+                            selectedTask?.description
+                        ) : (
+                            <span
+                                style={{
+                                    textAlign: "center",
+                                    width: "100%",
+                                }}
+                            >
+                                No Note
+                            </span>
+                        )}
+                    </p>
+                </div>
+
+                <div className={styles.expandFooter}>
+                    <ActionButton
+                        className={styles.backBtn}
+                        varient="secondary"
+                        onClick={() => changeAction("view")}
+                    >
+                        <span className="material-icons">arrow_back_ios</span>
+                    </ActionButton>
+                </div>
             </div>
         </main>
     );
