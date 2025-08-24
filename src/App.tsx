2f59691c3db748fa0a1e9cd48482e86d1fe0d95d
VasudevKishan <vasudevkishan@gmail.com>
2025-08-24 14:32:40 +0530

    code cleanup after redux setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './header';
 import ContentContainer from './content-container';
 import { ThemeProvider } from './context/ThemeContext';
-import { TaskProvider } from './context/TaskContext';
+
 import { SidebarProvider } from './context/SidebarContext';
 import { Provider } from 'react-redux';
 import { store } from './state/store';
@@ -12,12 +12,10 @@ function App() {
     <ThemeProvider>
       <SidebarProvider>
         <Provider store={store}>
-          <TaskProvider>
-            <>
-              <Header />
-              <ContentContainer />
-            </>
-          </TaskProvider>
+          <>
+            <Header />
+            <ContentContainer />
+          </>
         </Provider>
       </SidebarProvider>
     </ThemeProvider>
