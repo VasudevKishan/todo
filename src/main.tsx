e64968a690b9b49ac55c2591237332fa9815022a
Vasudev Kishan <vasudevkishan@gmail.com>
2026-01-14 13:28:59 +0530

    Disable devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+if (import.meta.env.VITE_ENV === 'production') {
+  disableReactDevTools();
+}
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
